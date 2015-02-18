b2d9a599053783cbf0b1a82d9c8aca52e1f010fc
mariano89 <marianoe1989@gmail.com>
2015-02-18 10:10:56 -0800

    added api key

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
+	
 	def index
-		
+		@resp = HTTParty.get("https://www.googleapis.com/youtube/v3/channels?part=contentDetails&mine=true&key=AIzaSyD473gmLA8qKL0aHbEuubsArlyQBho_lME")
 	end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
  
  root to: 'home#index'
 
- get '/home' => 'home#index'
+ # get '/home' => 'home#index'
 
 
 end
