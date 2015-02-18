9341cfd9ff445bf90ea2c8f8311697518b12dcbd
mariano89 <marianoe1989@gmail.com>
2015-02-18 10:28:21 -0800

    added title to video

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,10 @@ class HomeController < ApplicationController
 	def index
 		@resp = HTTParty.get("http://gdata.youtube.com/feeds/api/standardfeeds/most_popular?alt=json")
 		
-		@vid = @resp['feed']['entry'].sample['content']['src']
+		vid = @resp['feed']['entry'].sample
+
+		@vid = vid['content']['src']
+		@title = vid['title']['$t']
 	end
 
 
